5b5dd7710c2078290b5afd656811524a95dd7b46
Samuel Sciolla <ssciolla@umich.edu>
2024-07-11 12:31:48 -0400

    Add gradlew setup and storage structure; add beginning of RepositoryService with create and read

---
+++ app/src/main/java/org/example/RepositoryService.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.nio.file.Path;
+
+import io.ocfl.api.OcflRepository;
+import io.ocfl.api.model.ObjectVersionId;
+import io.ocfl.api.model.VersionInfo;
+import io.ocfl.core.OcflRepositoryBuilder;
+import io.ocfl.core.extension.storage.layout.config.HashedNTupleLayoutConfig;
+
+public class RepositoryService {
+
+    private OcflRepository repo;
+
+    public RepositoryService(Path rootPath, Path workPath) {
+        this.repo = new OcflRepositoryBuilder()
+            .prettyPrintJson()
+            .defaultLayoutConfig(new HashedNTupleLayoutConfig())
+            .storage(storage -> storage.fileSystem(rootPath))
+            .workDir(workPath)
+            .build();
+    }
+
+    public RepositoryService createObject(String id, Path inputPath, String message) {
+        repo.putObject(
+            ObjectVersionId.head(id),
+            inputPath,
+            new VersionInfo().setMessage(message)
+        );
+        return this;
+    }
+
+    public RepositoryService readObject(String id, Path outputPath) {
+        repo.getObject(ObjectVersionId.head(id), outputPath);
+        return this;
+    }
+
+    // public RepositoryService updateObject(...)
+    // }
+}
